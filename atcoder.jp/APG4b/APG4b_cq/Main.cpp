fa8c6903430da9d1ad2f7335ebb890f1b2986f3e
ult_saza <torimituyouiti@gmail.com>
2023-03-26 06:55:41 +0900

    [AC] APG4b APG4b_cq

---
+++ atcoder.jp/APG4b/APG4b_cq/Main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  string op;
+  cin >> A >> op >> B;
+
+  if (op == "+") {
+    cout << A + B << endl;
+  }
+  if (op == "-") {
+    cout << A - B << endl;
+  }
+  if (op == "*") {
+    cout << A * B << endl;
+  }
+  if (op == "/" && B == 0) {
+    cout << "error" << endl;
+  }
+  if (op == "/" && B != 0) {
+    cout << A / B << endl;
+  }
+  if (op == "?" || op == "=" || op == "!") {
+    cout << "error" << endl;
+  }
+    
+}
