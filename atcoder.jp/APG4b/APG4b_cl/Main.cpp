2c1ef47a60b0b9644a8a2b70da690f6a727b19e3
ult_saza <torimituyouiti@gmail.com>
2023-03-27 23:17:42 +0900

    [AC] APG4b APG4b_cl

---
+++ atcoder.jp/APG4b/APG4b_cl/Main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+	int main() {
+      int N,A; 
+      cin >> N >> A;
+      
+      for(int i=0;i<N;i++) {
+        int B; string op;
+        cin >> op >> B;
+        
+        if(op=="+") {
+          A+=B;
+        }
+        else if(op=="-") {
+          A-=B;
+        }
+        else if(op=="*") {
+          A*=B;
+        }
+        else if(op=="/" && B!=0) {
+          A/=B;
+        }
+        else {
+          cout << "error" << endl;
+          break;
+        }
+      cout << i+1 << ":" << A <<endl;
+      }
+    }
+    
