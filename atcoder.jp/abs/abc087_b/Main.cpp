70078ecca2a28383637d416b7682c316488624cb
ult_saza <torimituyouiti@gmail.com>
2023-04-14 21:43:51 +0900

    [AC] abs abc087_b

---
+++ atcoder.jp/abs/abc087_b/Main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+  int A,B,C,X; 
+  cin >> A >> B >> C >> X;
+  int count=0;
+  
+  for(int i=0;i<=A;i++) {
+    for(int j=0;j<=B;j++) {
+      for(int k=0;k<=C;k++) {
+        if(500*i+100*j+50*k==X) {
+          count++;
+        }
+      }
+    }
+  }
+  cout << count << endl;
+}
