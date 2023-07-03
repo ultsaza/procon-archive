45f1b18155a09bf2a6f9ea5f79459c033f8dfd5c
ult_saza <torimituyouiti@gmail.com>
2023-07-03 15:45:35 +0900

    [AC] abc308 abc308_b

---
+++ atcoder.jp/abc308/abc308_b/Main.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll=long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i= (s); i < (int)(n); i++)
+const ll M=1000000007;
+inline int scan(){ return getchar(); }
+inline void scan(int &a){ scanf("%d", &a); }
+inline void scan(unsigned &a){ scanf("%u", &a); }
+inline void scan(long &a){ scanf("%ld", &a); }
+inline void scan(long long &a){ scanf("%lld", &a); }
+inline void scan(unsigned long long &a){ scanf("%llu", &a); }
+inline void scan(char &a){ do{ a = getchar(); }while(a == ' ' ||a == '\n'); }
+inline void scan(float &a){ scanf("%f", &a); }
+inline void scan(double &a){ scanf("%lf", &a); }
+inline void scan(long double &a){ scanf("%Lf", &a); }
+inline void scan(vector<bool> &vec){ for(unsigned i = 0; i < vec.size(); i++) { int a; scan(a); vec[i] = a; } }
+inline void scan(char a[]){ scanf("%s", a); }
+inline void scan(string &a){ cin >> a; }
+template<class T> inline void scan(vector<T> &vec);
+template<class T, size_t size> inline void scan(array<T, size> &vec);
+template<class T, class L> inline void scan(pair<T, L> &p);
+template<class T, size_t size> inline void scan(T (&vec)[size]);
+template<class T> inline void scan(vector<T> &vec){ for(auto &i : vec) scan(i); }
+template<class T> inline void scan(deque<T> &vec){ for(auto &i : vec) scan(i); }
+template<class T, size_t size> inline void scan(array<T, size> &vec){ for(auto &i : vec) scan(i); }
+template<class T, class L> inline void scan(pair<T, L> &p){ scan(p.first); scan(p.second); }
+template<class T, size_t size> inline void scan(T (&vec)[size]){ for(auto &i : vec) scan(i); }
+template<class T> inline void scan(T &a){ cin >> a; }
+inline void in(){}
+template <class Head, class... Tail> inline void in(Head &head, Tail&... tail){ scan(head); in(tail...); }
+inline void print(){ putchar(' '); }
+inline void print(const bool &a){ printf("%d", a); }
+inline void print(const int &a){ printf("%d", a); }
+inline void print(const unsigned &a){ printf("%u", a); }
+inline void print(const long &a){ printf("%ld", a); }
+inline void print(const long long &a){ printf("%lld", a); }
+inline void print(const unsigned long long &a){ printf("%llu", a); }
+inline void print(const char &a){ printf("%c", a); }
+inline void print(const char a[]){ printf("%s", a); }
+inline void print(const float &a){ printf("%.15f", a); }
+inline void print(const double &a){ printf("%.15f", a); }
+inline void print(const long double &a){ printf("%.15Lf", a); }
+inline void print(const string &a){ for(auto&& i : a) print(i); }
+template<class T> inline void print(const vector<T> &vec);
+template<class T, size_t size> inline void print(const array<T, size> &vec);
+template<class T, class L> inline void print(const pair<T, L> &p);
+template<class T, size_t size> inline void print(const T (&vec)[size]);
+template<class T> inline void print(const vector<T> &vec){ if(vec.empty()) return; print(vec[0]); for(auto i = vec.begin(); ++i != vec.end(); ){ putchar(' '); print(*i); } }
+template<class T> inline void print(const deque<T> &vec){ if(vec.empty()) return; print(vec[0]); for(auto i = vec.begin(); ++i != vec.end(); ){ putchar(' '); print(*i); } }
+template<class T, size_t size> inline void print(const array<T, size> &vec){ print(vec[0]); for(auto i = vec.begin(); ++i != vec.end(); ){ putchar(' '); print(*i); } }
+template<class T, class L> inline void print(const pair<T, L> &p){ print(p.first); putchar(' '); print(p.second); }
+template<class T, size_t size> inline void print(const T (&vec)[size]){ print(vec[0]); for(auto i = vec; ++i != end(vec); ){ putchar(' '); print(*i); } }
+template<class T> inline void print(const T &a){ cout << a; }
+inline int out(){ putchar('\n'); return 0; }
+template<class T> inline int out(const T &t){ print(t); putchar('\n'); return 0; }
+template<class Head, class... Tail> inline int out(const Head &head, const Tail&... tail){ print(head); putchar(' '); out(tail...); return 0; }
+inline int first(bool i = true){ return out(i?"first":"second"); }
+inline int yes(bool i = true){ return out(i?"yes":"no"); }
+inline int Yes(bool i = true){ return out(i?"Yes":"No"); }
+inline int No(){ return out("No"); }
+inline int YES(bool i = true){ return out(i?"YES":"NO"); }
+inline int NO(){ return out("NO"); }
+inline int Yay(bool i = true){ return out(i?"Yay!":":("); }
+inline int Possible(bool i = true){ return out(i?"Possible":"Impossible"); }
+inline int POSSIBLE(bool i = true){ return out(i?"POSSIBLE":"IMPOSSIBLE"); }
+inline void Case(ll i){ printf("Case #%lld: ", i); }
+
+int main() {
+    int n,m;
+    in(n,m);
+    vector<string> c(n);
+    vector<string> d(m);
+    vector<ll> p(m+1);
+    in(c,d,p);
+    ll ans=0;
+    rep(i,n) {
+        bool b=0;
+        rep(j,m) {
+            if(c[i]==d[j] && b==0) {
+                ans+=p[j+1];
+                b=1;
+            }
+        }
+        if(b==0) ans+=p[0];
+    }
+    out(ans);
+}
