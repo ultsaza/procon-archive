80db43cc18b4d741484d0244728ac5538cacdc33
ult_saza <torimituyouiti@gmail.com>
2023-07-09 16:41:49 +0900

    [AC] tessoku-book math_and_algorithm_an

---
+++ atcoder.jp/tessoku-book/math_and_algorithm_an/Main.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll=long long;
+#define overload4(_1, _2, _3, _4, name, ...) name
+#define rep1(n) for(ll i = 0; i < (n); ++i)
+#define rep2(i, n) for(ll i = 0; i < (n); ++i)
+#define rep3(i, a, b) for(ll i = (a); i < (b); ++i)
+#define rep4(i, a, b, c) for(ll i = (a); i < (b); i += (c))
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+const ll LINF = 0x1fffffffffffffff;
+inline int scan(){ return getchar(); }
+inline void scan(int &a){ scanf("%d", &a); }
+inline void scan(unsigned &a){ scanf("%u", &a); }
+inline void scan(long &a){ scanf("%ld", &a); }
+inline void scan(long long &a){ scanf("%lld", &a); }
+inline void scan(unsigned long long &a){ scanf("%llu", &a); }
+inline void scan(char &a){ do{ a = getchar(); }while(a == ' ' ||a == '\n'); }
+inline void scan(float &a){ scanf("%f", &a); }
+inline void scan(double &a){ scanf("%lf", &a); }
+inline void scan(long double &a){ scanf("%Lf", &a); }
+inline void scan(vector<bool> &vec){ for(unsigned i = 0; i < vec.size(); i++) { int a; scan(a); vec[i] = a; } }
+inline void scan(char a[]){ scanf("%s", a); }
+inline void scan(string &a){ cin >> a; }
+template<class T> inline void scan(vector<T> &vec);
+template<class T, size_t size> inline void scan(array<T, size> &vec);
+template<class T, class L> inline void scan(pair<T, L> &p);
+template<class T, size_t size> inline void scan(T (&vec)[size]);
+template<class T> inline void scan(vector<T> &vec){ for(auto &i : vec) scan(i); }
+template<class T> inline void scan(deque<T> &vec){ for(auto &i : vec) scan(i); }
+template<class T, size_t size> inline void scan(array<T, size> &vec){ for(auto &i : vec) scan(i); }
+template<class T, class L> inline void scan(pair<T, L> &p){ scan(p.first); scan(p.second); }
+template<class T, size_t size> inline void scan(T (&vec)[size]){ for(auto &i : vec) scan(i); }
+template<class T> inline void scan(T &a){ cin >> a; }
+inline void in(){}
+template <class Head, class... Tail> inline void in(Head &head, Tail&... tail){ scan(head); in(tail...); }
+inline void print(){ putchar(' '); }
+inline void print(const bool &a){ printf("%d", a); }
+inline void print(const int &a){ printf("%d", a); }
+inline void print(const unsigned &a){ printf("%u", a); }
+inline void print(const long &a){ printf("%ld", a); }
+inline void print(const long long &a){ printf("%lld", a); }
+inline void print(const unsigned long long &a){ printf("%llu", a); }
+inline void print(const char &a){ printf("%c", a); }
+inline void print(const char a[]){ printf("%s", a); }
+inline void print(const float &a){ printf("%.15f", a); }
+inline void print(const double &a){ printf("%.15f", a); }
+inline void print(const long double &a){ printf("%.15Lf", a); }
+inline void print(const string &a){ for(auto&& i : a) print(i); }
+template<class T> inline void print(const vector<T> &vec);
+template<class T, size_t size> inline void print(const array<T, size> &vec);
+template<class T, class L> inline void print(const pair<T, L> &p);
+template<class T, size_t size> inline void print(const T (&vec)[size]);
+template<class T> inline void print(const vector<T> &vec){ if(vec.empty()) return; print(vec[0]); for(auto i = vec.begin(); ++i != vec.end(); ){ putchar(' '); print(*i); } }
+template<class T> inline void print(const deque<T> &vec){ if(vec.empty()) return; print(vec[0]); for(auto i = vec.begin(); ++i != vec.end(); ){ putchar(' '); print(*i); } }
+template<class T, size_t size> inline void print(const array<T, size> &vec){ print(vec[0]); for(auto i = vec.begin(); ++i != vec.end(); ){ putchar(' '); print(*i); } }
+template<class T, class L> inline void print(const pair<T, L> &p){ print(p.first); putchar(' '); print(p.second); }
+template<class T, size_t size> inline void print(const T (&vec)[size]){ print(vec[0]); for(auto i = vec; ++i != end(vec); ){ putchar(' '); print(*i); } }
+template<class T> inline void print(const T &a){ cout << a; }
+inline int out(){ putchar('\n'); return 0; }
+template<class T> inline int out(const T &t){ print(t); putchar('\n'); return 0; }
+template<class Head, class... Tail> inline int out(const Head &head, const Tail&... tail){ print(head); putchar(' '); out(tail...); return 0; }
+
+
+int main() {
+    int N,M;
+    vector<int> A(100009);
+    vector<int> B(100009);
+    vector<int> dist(100009);
+    vector<vector<int>> G(100009,vector<int> ());
+    queue<int> Q;
+    in(N,M);
+    rep(i,M) {
+        in(A[i],B[i]); A[i]--;B[i]--;
+        G[A[i]].push_back(B[i]);
+        G[B[i]].push_back(A[i]);
+    }
+    rep(i,1,N) dist[i]=-1;
+    dist[0]=0;
+    Q.push(0);
+    while(!Q.empty()) {
+        int pos = Q.front();Q.pop();
+        rep(i,G[pos].size()){
+            int to=G[pos][i];
+            if(dist[to]== -1) {
+                dist[to]=dist[pos]+1;
+                Q.push(to);
+            }
+        }
+    }
+    rep(i,N) out(dist[i]);
+}
