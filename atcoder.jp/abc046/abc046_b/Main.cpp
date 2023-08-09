56673e094c210c6508d9117e1fe3dae0628fbb7c
ult_saza <torimituyouiti@gmail.com>
2023-08-09 13:18:24 +0900

    [AC] abc046 abc046_b

---
+++ atcoder.jp/abc046/abc046_b/Main.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll=long long;
+#define all(v) v.begin(), v.end()
+#define overload4(_1, _2, _3, _4, name, ...) name
+#define rep1(n) for(ll i = 0; i < (n); ++i)
+#define rep2(i, n) for(ll i = 0; i < (n); ++i)
+#define rep3(i, a, b) for(ll i = (a); i < (b); ++i)
+#define rep4(i, a, b, c) for(ll i = (a); i < (b); i += (c))
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+const ll LINF = 0x1fffffffffffffff;
+
+int main() {
+    ll n,k; cin >> n >> k;
+    cout << (ll)(k*pow(k-1,n-1));
+}
