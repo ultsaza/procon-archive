a38bb8aedd3e6d92bc5ea588bf493bf61b8ad024
ult_saza <torimituyouiti@gmail.com>
2023-08-13 18:22:59 +0900

    [AC] abc042 abc042_a

---
+++ atcoder.jp/abc042/abc042_a/Main.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll=long long;
+#define all(v) v.begin(), v.end()
+#define overload4(_1, _2, _3, _4, name, ...) name
+#define rep1(n) for(ll i = 0; i < (n); ++i)
+#define rep2(i, n) for(ll i = 0; i < (n); ++i)
+#define rep3(i, a, b) for(ll i = (a); i < (b); ++i)
+#define rep4(i, a, b, c) for(ll i = (a); i < (b); i += (c))
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+const ll LINF = 0x1fffffffffffffff;
+
+int main(void) {
+    int a,b,c; cin >> a >> b >> c;
+    if(a*b*c==175) cout << "YES" << '\n'; else cout << "NO" << '\n';
+    return 0;
+}
