634b9b98b433c33116f5b51158eadc8bc147deb1
ult_saza <torimituyouiti@gmail.com>
2023-08-19 15:15:41 +0900

    [AC] abc213 abc213_a

---
+++ atcoder.jp/abc213/abc213_a/Main.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll=long long;
+#define overload4(_1, _2, _3, _4, name, ...) name
+#define rep1(n) for(ll i = 0; i < (n); ++i)
+#define rep2(i, n) for(ll i = 0; i < (n); ++i)
+#define rep3(i, a, b) for(ll i = (a); i < (b); ++i)
+#define rep4(i, a, b, c) for(ll i = (a); i < (b); i += (c))
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define all(i) begin(i), end(i)
+const ll LINF = 0x1fffffffffffffff;
+
+
+int main(void) {
+    bitset<8> a,b;
+    int x,y; cin >> x >> y;
+    a=x; b=y;
+    rep(i,256) {
+        bitset<8> c;
+        c=i;
+        if((a^c)==b) cout << i;
+    }
+}
