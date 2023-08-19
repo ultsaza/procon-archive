181d493ff7c35dc15d3e5e35380b0aa86e299e07
ult_saza <torimituyouiti@gmail.com>
2023-08-19 15:23:38 +0900

    [AC] abc213 abc213_b

---
+++ atcoder.jp/abc213/abc213_b/Main.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll=long long;
+#define overload4(_1, _2, _3, _4, name, ...) name
+#define rep1(n) for(ll i = 0; i < (n); ++i)
+#define rep2(i, n) for(ll i = 0; i < (n); ++i)
+#define rep3(i, a, b) for(ll i = (a); i < (b); ++i)
+#define rep4(i, a, b, c) for(ll i = (a); i < (b); i += (c))
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define all(i) begin(i), end(i)
+const ll LINF = 0x1fffffffffffffff;
+
+
+int main(void) {
+    int n;cin>>n;
+    vector<pair<int,int>> A(n);
+    rep(i,n) {
+        int a; cin >> a;
+        A[i] = {a,i};
+    }
+    sort(all(A));
+    reverse(all(A));
+    cout << A[1].second+1 << '\n';
+}
