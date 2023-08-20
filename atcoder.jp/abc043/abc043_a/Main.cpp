b7b9d81a27b41cb34a2e5edff4bfd63a29e137f2
ult_saza <torimituyouiti@gmail.com>
2023-08-20 13:54:58 +0900

    [AC] abc043 abc043_a

---
+++ atcoder.jp/abc043/abc043_a/Main.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll=long long;
+#define overload4(_1, _2, _3, _4, name, ...) name
+#define rep1(n) for(ll i = 0; i < (n); ++i)
+#define rep2(i, n) for(ll i = 0; i < (n); ++i)
+#define rep3(i, a, b) for(ll i = (a); i < (b); ++i)
+#define rep4(i, a, b, c) for(ll i = (a); i < (b); i += (c))
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define all(i) begin(i), end(i)
+const int INF = 0x3fffffff;
+const ll LINF = 0x1fffffffffffffff;
+
+
+int main() {
+   int n;cin >> n;
+   cout << n*(n+1)/2 << endl;
+}
