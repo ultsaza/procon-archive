3cfe50b855b7bb9fa773311f6296a6c7fcb7b695
ult_saza <torimituyouiti@gmail.com>
2023-08-28 17:05:52 +0900

    [AC] practice2 practice2_g

---
+++ atcoder.jp/practice2/practice2_g/Main.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll=long long;
+#define overload4(_1, _2, _3, _4, name, ...) name
+#define rep1(n) for(ll i = 0; i < (n); ++i)
+#define rep2(i, n) for(ll i = 0; i < (n); ++i)
+#define rep3(i, a, b) for(ll i = (a); i < (b); ++i)
+#define rep4(i, a, b, c) for(ll i = (a); i < (b); i += (c))
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define all(i) begin(i), end(i)
+const int INF = 0x3fffffff;
+const ll LINF = 0x1fffffffffffffff;
+
+
+int main() {
+    int N,M; cin >> N >> M;
+    scc_graph G(N);
+    rep(i,M) {
+        int a,b; cin >> a >> b;
+        G.add_edge(a,b);
+    }
+    vector<vector<int>> ans;
+    ans = G.scc();
+    int s =ans.size();
+    cout << s << endl;
+    rep(i,s) {
+        cout << ans[i].size() << " ";
+        rep(j,ans[i].size()) cout << ans[i][j] << " ";
+        cout << endl;
+    }
+}
