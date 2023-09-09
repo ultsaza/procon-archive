38c60e128bfb8ee156e0304f37de0ae01d8802e6
ult_saza <torimituyouiti@gmail.com>
2023-09-09 20:58:26 +0900

    [AC] abc288 abc288_c

---
+++ atcoder.jp/abc288/abc288_c/Main.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll=long long;
+#define overload4(_1, _2, _3, _4, name, ...) name
+#define rep1(n) for(ll i = 0; i < (n); ++i)
+#define rep2(i, n) for(ll i = 0; i < (n); ++i)
+#define rep3(i, a, b) for(ll i = (a); i < (b); ++i)
+#define rep4(i, a, b, c) for(ll i = (a); i < (b); i += (c))
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define all(i) begin(i), end(i)
+const int dx[4] = {1,0,-1,0};
+const int dy[4] = {0,1,0,-1};
+const int INF = 0x3fffffff;
+const ll LINF = 0x1fffffffffffffff;
+
+int main() {
+    int N,M; cin >> N >> M;
+    dsu uf(N);
+    int ans=0;
+    rep(i,M) {
+        int a,b; cin >> a >> b;
+        a--;b--;
+        if(uf.same(a,b)) ans++; //消せる辺
+        uf.merge(a,b);
+    }
+    cout << ans;
+}
