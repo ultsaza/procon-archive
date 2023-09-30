aee8d674cee4249f135b42069c4b0f8188ed616d
ult_saza <torimituyouiti@gmail.com>
2023-09-30 21:02:32 +0900

    [AC] abc322 abc322_a

---
+++ atcoder.jp/abc322/abc322_a/Main.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll=long long;
+#define overload4(_1, _2, _3, _4, name, ...) name
+#define rep1(n) for(ll i = 0; i < (n); ++i)
+#define rep2(i, n) for(ll i = 0; i < (n); ++i)
+#define rep3(i, a, b) for(ll i = (a); i < (b); ++i)
+#define rep4(i, a, b, c) for(ll i = (a); i < (b); i += (c))
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define all(i) begin(i), end(i)
+const int INF = 0x3fffffff;
+const ll LINF = 0x1fffffffffffffff;
+
+
+
+int main() {
+    int N; string S;
+    cin >> N >> S;
+    rep(i,N-2) {
+        if(S[i]=='A'&&S[i+1]=='B'&&S[i+2]=='C') {
+            cout << i+1 << endl;
+            return 0;
+        }
+    }
+    cout << -1;
+}
