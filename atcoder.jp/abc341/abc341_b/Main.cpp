2c9bebea7ba19fc7c93002d01eae2b154c1c36ca
ult_saza <torimituyouiti@gmail.com>
2024-02-17 21:10:21 +0900

    [AC] abc341 abc341_b

---
+++ atcoder.jp/abc341/abc341_b/Main.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll=long long;
+#define overload4(_1, _2, _3, _4, name, ...) name
+#define rep1(n) for(ll i = 0; i < (n); ++i)
+#define rep2(i, n) for(ll i = 0; i < (n); ++i)
+#define rep3(i, a, b) for(ll i = (a); i < (b); ++i)
+#define rep4(i, a, b, c) for(ll i = (a); i < (b); i += (c))
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define all(i) begin(i), end(i)
+const int INF = 0x3fffffff;
+const ll LINF = 0x1ffffffffffffff;
+
+
+int main() {
+    ll N; cin >> N;
+    vector<ll> A(N);
+    rep(N) cin >> A[i];
+    rep(i,N-1) {
+        ll s,t;
+        cin >> s >> t;
+        if(s <= A[i]) {
+            A[i+1] += (A[i]/s)*t;
+        }
+    }
+    cout << A[N-1] << endl;
+    
+}
