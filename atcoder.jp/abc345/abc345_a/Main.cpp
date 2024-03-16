31fd8aeceef934a2e27f4830eca43234af417ceb
ult_saza <torimituyouiti@gmail.com>
2024-03-16 21:02:18 +0900

    [AC] abc345 abc345_a

---
+++ atcoder.jp/abc345/abc345_a/Main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+#define overload4(_1, _2, _3, _4, name, ...) name
+#define rep1(n) for (ll i = 0; i < (n); ++i)
+#define rep2(i, n) for (ll i = 0; i < (n); ++i)
+#define rep3(i, a, b) for (ll i = (a); i < (b); ++i)
+#define rep4(i, a, b, c) for (ll i = (a); i < (b); i += (c))
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define all(i) begin(i), end(i)
+const int INF = 0x3fffffff;
+const ll LINF = 0x1ffffffffffffff;
+
+
+int main(){
+    string S;
+    cin >> S;
+    if(!(S[0]== '<')) {
+        cout << "No" << endl;
+        return 0;
+    }
+    rep(i, 1, S.size()-1) {
+        if(!(S[i]=='=')) {
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    if(!(S[S.size()-1] == '>')) {
+        cout << "No" << endl;
+        return 0;
+    }
+    cout << "Yes" << endl;
+}
