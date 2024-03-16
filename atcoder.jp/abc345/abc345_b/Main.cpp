b4b0f066f9b8f3e93bd7775ea74390ca20da6f2b
ult_saza <torimituyouiti@gmail.com>
2024-03-16 21:14:45 +0900

    [AC] abc345 abc345_b

---
+++ atcoder.jp/abc345/abc345_b/Main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+#define overload4(_1, _2, _3, _4, name, ...) name
+#define rep1(n) for (ll i = 0; i < (n); ++i)
+#define rep2(i, n) for (ll i = 0; i < (n); ++i)
+#define rep3(i, a, b) for (ll i = (a); i < (b); ++i)
+#define rep4(i, a, b, c) for (ll i = (a); i < (b); i += (c))
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define all(i) begin(i), end(i)
+const int INF = 0x3fffffff;
+const ll LINF = 0x1ffffffffffffff;
+
+
+
+
+
+int main(){
+    ll n;
+    cin >> n;   
+    if(n%10 ==0) {
+        cout << n/10 << endl;
+    }
+    else if(n>0) {
+        cout << n/10 + 1 << endl;
+    }
+    else {
+        cout << n/10 << endl;
+    }
+}
