827f76194e13ae586829ca58f29a09a5bc2f4c80
ult_saza <torimituyouiti@gmail.com>
2024-04-19 16:42:59 +0900

    [AC] abc341 abc341_a

---
+++ atcoder.jp/abc341/abc341_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+#define rep(i, l, r) for (ll i = (l); i < (r); ++i)
+#define all(x) (x).begin(), (x).end()
+const ll INF = 0x1ffffffffffffff;
+
+int main() {
+    int N;
+    cin >> N;
+    rep(i,0,N) {
+        cout << 1 << 0;
+    }
+    cout << 1;
+}
