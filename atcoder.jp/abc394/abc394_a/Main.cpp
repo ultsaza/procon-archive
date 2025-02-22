f17614bdfb0e2a4c09b75caf0c995496cf68231f
ult_saza <torimituyouiti@gmail.com>
2025-02-23 03:45:34 +0900

    [AC] abc394 abc394_a

---
+++ atcoder.jp/abc394/abc394_a/Main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define rep(i, l, r) for (ll i = (l); i < (r); ++i)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using ld = long double;
+using pl = pair<ll,ll>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+using vld = vector<ld>;
+bool chmin(auto& a, auto b) { return a > b ? a = b, 1 : 0; }
+bool chmax(auto& a, auto b) { return a < b ? a = b, 1 : 0; }
+//#define _GLIBCXX_DEBUG
+
+
+int main() {
+    string s;
+    cin >> s;
+    string t = "";
+    for (char c : s) {
+        if (c == '2') t += c;
+    }
+    cout << t << endl;
+}
