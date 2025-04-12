9b6d12a8653cb6a68fe181da43bbdf74b3afccd9
ult_saza <torimituyouiti@gmail.com>
2025-04-13 01:55:58 +0900

    [AC] abc190 abc190_d

---
+++ atcoder.jp/abc190/abc190_d/Main.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define rep(i, l, r) for (ll i = (l); i < (r); ++i)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using pl = pair<ll,ll>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+using mint=modint998244353;
+#define INF 1e18 
+//#pragma GCC target("avx2")
+//#pragma GCC optimize("O3")
+//#pragma GCC optimize("unroll-loops")
+
+
+// 素因数分解
+template <typename T>
+map<T, T> prime_factor(T n) {
+    map<T, T> ret;
+    for (T i = 2; i * i <= n; i++) {
+        T tmp = 0;
+        while (n % i == 0) {
+            tmp++;
+            n /= i;
+        }
+        ret[i] = tmp;
+    }
+    if (n != 1) ret[n] = 1;
+    return ret;
+}
+/*  divisor_num(n)
+    入力：整数 n
+    出力：nの約数の個数
+    計算量：O(√n)
+*/
+template <typename T>
+T divisor_num(T N) {
+    map<T, T> pf = prime_factor(N);
+    T ret = 1;
+    for (auto p : pf) {
+        
+        ret *= (p.second + 1);
+    }
+    return ret;
+}
+
+int main() {
+    ll n;
+    cin >> n;
+    while(n % 2 == 0) n /= 2;
+    cout << divisor_num(n) * 2 << endl;
+
+}
