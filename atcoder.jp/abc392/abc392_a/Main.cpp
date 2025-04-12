954817bec04c40590bf7e20f3b55c91b13a2ef27
ult_saza <torimituyouiti@gmail.com>
2025-04-13 05:02:21 +0900

    [AC] abc392 abc392_a

---
+++ atcoder.jp/abc392/abc392_a/Main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define rep(i, l, r) for (ll i = (l); i < (r); ++i)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using pl = pair<ll,ll>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+using mint=modint998244353;
+#define INF 1e18 
+//#pragma GCC target("avx2")
+//#pragma GCC optimize("O3")
+//#pragma GCC optimize("unroll-loops")
+
+
+int main() {
+    vl a(3); cin >> a[0] >> a[1] >> a[2];
+    sort(all(a));
+    cout << (a[0]*a[1] == a[2] ? "Yes" : "No") << endl;
+}
