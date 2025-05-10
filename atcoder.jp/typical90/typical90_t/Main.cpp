c95af4b37027bbd04a6a9e3b402071e7e9024c79
ult_saza <torimituyouiti@gmail.com>
2025-05-11 04:37:28 +0900

    [AC] typical90 typical90_t

---
+++ atcoder.jp/typical90/typical90_t/Main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define rep(i, l, r) for (ll i = (l); i < (r); ++i)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pl = pair<ll,ll>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+using ld = long double;
+//#pragma GCC target("avx2")
+//#pragma GCC optimize("O3")
+//#pragma GCC optimize("unroll-loops")
+using mint=modint1000000007;
+const ll INF = 1e18;
+
+int main() {
+    ll a,b,c;
+    cin >> a >> b >> c;
+    ll cb = 1;
+    rep(i,0,b) {
+        cb *= c;
+        if (cb > a) {
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
+}
