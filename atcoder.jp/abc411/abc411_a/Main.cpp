4ecebb0b9aeb041787f998f04e12e2e11c1941a9
ult_saza <torimituyouiti@gmail.com>
2025-06-21 21:01:37 +0900

    [AC] abc411 abc411_a

---
+++ atcoder.jp/abc411/abc411_a/Main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define rep(i, l, r) for (ll i = (l); i < (r); ++i)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pl = pair<ll,ll>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+using ld = long double;
+//#pragma GCC target("avx2")
+//#pragma GCC optimize("O3")
+//#pragma GCC optimize("unroll-loops")
+using mint=modint1000000007;
+const ll INF = 1e18;
+
+int main() {
+    string s;
+    int l;
+    cin >> s >> l;
+    if (s.size() >= l) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
